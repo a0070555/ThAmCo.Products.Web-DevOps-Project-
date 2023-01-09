204a98320f225dba4e8084acc1542d56db72564c
rushd <rushd@DESKTOP-P2R8P41>
2023-01-09 12:52:09 +0000

    Added polly and initialiser

--- ThAmCo.Products.Web/Program.cs
+++ ThAmCo.Products.Web/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Polly.Extensions.Http;
+using Polly;
+using Thamco.Products.Web.Data;
 using ThAmCo.Products.Web.Data;
 using ThAmCo.Products.Web.Repositories;
 
@@ -10,9 +13,9 @@ if (builder.Environment.IsDevelopment())
 }
 else
 {
-    builder.Services.AddHttpClient<IProductsRepository, ProductsRepository>();
-        //.AddPolicyHandler(GetRetryPolicy())
-        //.AddPolicyHandler(GetCircuitBreakerPolicy());
+    builder.Services.AddHttpClient<IProductsRepository, ProductsRepository>()
+        .AddPolicyHandler(GetRetryPolicy())
+        .AddPolicyHandler(GetCircuitBreakerPolicy());
 }
 
 builder.Services.AddDbContext<ProductsContext>(options =>
@@ -45,6 +48,25 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    if (app.Environment.IsDevelopment())
+    {
+        var context = services.GetRequiredService<ProductsContext>();
+        context.Database.Migrate();
+        try
+        {
+            ProductsInitialiser.InsertTestData(context).Wait();
+        }
+        catch (Exception e)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogDebug("Inserting test data failed.");
+        }
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -65,3 +87,19 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+        .WaitAndRetryAsync(5, retryAttempt =>
+            TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+}
+
+IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
+}
