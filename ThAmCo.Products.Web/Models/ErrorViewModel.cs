be3150551858359aa05a182db811f06ebe613bf3
rushd <rushd@DESKTOP-P2R8P41>
2023-01-09 12:23:48 +0000

    Add project files.

---
+++ ThAmCo.Products.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThAmCo.Products.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ThAmCo.Products.Web/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThAmCo.Products.Web.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Type { get; set; } //Two types: "Standard" "Custom"
+
+        [Required]
+        public string? ProductName { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public string? Description { get; set; }
+    }
+
+}
